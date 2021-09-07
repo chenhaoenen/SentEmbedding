23f01241288a045a77712bda0ce8fcbd9c35d544
Chen-Hao-fc <chenhao_fc@163.com>
2021-09-07 20:53:50 +0800

    +

---
+++ doc/fastsent.md
@@ -0,0 +1,27 @@
+## 简介 
+paper: [Learning Distributed Representations of Sentences from Unlabelled Data](https://arxiv.org/abs/1602.03483)
+
+code: https://github.com/fh295/SentenceRepresentation
+
+## method  
+
+### 摘要  
+1. Deeper, more complex models are preferable for representations to be used in supervised systems,
+2. shallow log-linear models work best for building representation spaces that can be decoded with simple spatial distance metrics.(感觉说的是无监督)
+### 提了两个方法
+1. SDAE(Sequential Denoising Autoencoders)
+2. FastSent
+
+### SDAE
+从DAE(denoising autoencoders)衍化而来，其实就是autoencoder,给序列加点噪音，然后recover the original data,该方法常用与CV领域，
+文章提供了两种noise function,第一种是以一定概率删除部分token， 第二种是以一定的概率swap两个token,然后recover the original data
+源代码我没来得及看,因为是基于theano框架的
+### FastSent
+该方案是基于SkipThought vector衍化而来，论文之处SkipThought的缺点是训练缓慢，改善的方法是使用BOW的方案。具体的公式如下：
+$$
+\begin{align}
+\sum_{w \in S_{i-1} \cup S_{i+1}} \phi\left(\mathbf{s}_{\mathbf{i}}, v_{w}\right)
+\end{align}
+$$
+
+
--- src/model/bart/model.py
+++ src/model/bart/model.py
@@ -10,16 +10,49 @@ from typing import Optional
 from transformers.models.bart.modeling_bart import (
     BartConfig,
     BartEncoderLayer,
+    BartDecoderLayer,
     PretrainedBartModel,
     BartLearnedPositionalEmbedding,
 )
 
 class ParaBART(PretrainedBartModel):
-    def __init__(self, config):
+    '''Reference https://github.com/uclanlp/ParaBART '''
+    def __init__(self, config: BartConfig):
         super(ParaBART, self).__init__(config)
 
-    def forward(self):
-        pass
+        padding_idx, vocab_size = config.pad_token_id, config.vocab_size
+        self.shared = nn.Embedding(vocab_size, config.d_model, padding_idx)
+
+        self.encoder = ParaBartEncoder(config, self.shared)
+        self.decoder = ParaBartDecoder(config, self.shared)
+
+
+        self.init_weights()
+
+    def forward(self,
+                input_ids,
+                attention_mask,
+                decoder_input_ids,
+                decoder_attention_mask=None,
+                encoder_outputs=None,
+                return_encoder_outputs=False):
+
+        if attention_mask is None:
+            attention_mask = input_ids == self.config.pad_token_id
+        if encoder_outputs is None:
+            encoder_outputs = self.encoder(input_ids, attention_mask=attention_mask)
+
+        if return_encoder_outputs:
+            return encoder_outputs
+
+        assert encoder_outputs is not None
+        assert decoder_input_ids is not None
+
+        decoder_input_ids = decoder_input_ids[:, :-1]
+
+        decoder_outputs = self.decoder(input_ids=decoder_input_ids,
+                                       attention_mask=decoder_attention_mask,
+                                       )
 
 
 class ParaBartEncoder(PretrainedBartModel):
@@ -51,6 +84,8 @@ class ParaBartEncoder(PretrainedBartModel):
         self.synt_layers = nn.ModuleList([BartEncoderLayer(config) for _ in range(1)])
         self.synt_layernorm_embedding = nn.LayerNorm(embed_dim)
 
+        self.pooling = MeanPooling(config)
+
     def forward(self, token_input_ids=None, token_attention_mask=None, synt_input_ids=None, synt_attention_mask=None):
 
         if self.training:
@@ -65,6 +100,89 @@ class ParaBartEncoder(PretrainedBartModel):
         token_hidden_states = token_inputs_embeds + token_embed_pos
         token_hidden_states = self.layernorm_embedding(token_hidden_states)
         token_hidden_states = nn.functional.dropout(token_hidden_states, p=self.dropout, training=self.training)
+
+        for idx, encoder_layer in enumerate(self.layers):
+            token_layer_outputs = encoder_layer(token_hidden_states, attention_mask=token_attention_mask)
+            token_hidden_states = token_layer_outputs[0]
+
+        # syntactic
+        synt_inputs_embeds = self.embed_synt(synt_input_ids)
+        synt_input_shape = synt_input_ids.size()
+        synt_embed_pos = self.embed_positions(synt_input_shape)
+        synt_hidden_states = synt_inputs_embeds + synt_embed_pos
+        synt_hidden_states = self.synt_layernorm_embedding(synt_hidden_states)
+        synt_hidden_states = nn.functional.dropout(synt_hidden_states, p=self.dropout, training=self.training)
+
+        for idx, encoder_layer in enumerate(self.synt_layers):
+            synt_layer_outputs = encoder_layer(synt_hidden_states, attention_mask=synt_attention_mask)
+            synt_hidden_states = synt_layer_outputs[0]
+
+        encoder_outputs = torch.cat([token_hidden_states, synt_hidden_states], dim=1)
+
+        sent_embeds = self.pooling(token_hidden_states, token_input_ids)
+
+        return encoder_outputs, sent_embeds
+
+
+class ParaBartDecoder(PretrainedBartModel):
+    def __init__(self, config:BartConfig, embed_tokens:Optional[nn.Embedding]=None):
+        super().__init__(config)
+        self.dropout = config.dropout
+        self.layerdrop = config.decoder_layerdrop
+        embed_dim = config.d_model
+
+        self.padding_idx = config.pad_token_id
+        self.max_target_positions = config.max_position_embeddings
+
+        self.embed_tokens = embed_tokens
+        self.embed_positions = BartLearnedPositionalEmbedding(self.max_source_positions, embed_dim, config.pad_token_id)
+
+        self.layers = nn.ModuleList([BartDecoderLayer(config) for _ in range(1)])
+        self.layernorm_embedding = nn.LayerNorm(embed_dim)
+
+    def forward(self,
+                input_ids=None,
+                attention_mask=None,
+                encoder_hidden_states=None,
+                encoder_attention_mask=None,
+                decoder_cross_attn_head_mask=None):
+
+        inputs_embeds = self.embed_tokens(input_ids)
+        input_shape = input_ids.size()
+        embed_pos = self.embed_positions(input_shape)
+        hidden_states = inputs_embeds + embed_pos
+        hidden_states = self.layernorm_embedding(hidden_states)
+        hidden_states = nn.functional.dropout(hidden_states, p=self.dropout, training=self.training)
+
+        for idx, decoder_layer in enumerate(self.layers):
+            layer_outputs = decoder_layer(hidden_states,
+                                          attention_mask=attention_mask,
+                                          encoder_hidden_states=encoder_hidden_states,
+                                          encoder_attention_mask=encoder_attention_mask,
+                                          cross_attn_layer_head_mask=decoder_cross_attn_head_mask)
+            hidden_states = layer_outputs[0]
+
+
+        return hidden_states
+
+
+class MeanPooling(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+
+    def forward(self, token_hidden_states, token_input_ids):
+        mask = token_input_ids == self.config.pad_token_id
+        mean_mask = mask.float() / mask.float().sum(1, keepdim=True)
+        token_hidden_states = token_hidden_states * mean_mask.unsqueeze(2).sum(dim=1, keepdim=True)
+        return token_hidden_states
+
+
+
+
+
+
+
 
 
 
